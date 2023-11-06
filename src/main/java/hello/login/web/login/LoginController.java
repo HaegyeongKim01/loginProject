4f6e202ab4e7ac2910e02ad8125cf4e8bc00cd49
HaegyeongKim <Haegyeon.kim01@gmail.com>
2023-11-06 17:36:48 +0900

    Login-Session

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
@@ -19,14 +21,15 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
-    public String login(@ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+//    @PostMapping("/login")
+    public String loginCookie(@ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
 
         if(bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -47,9 +50,37 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletResponse response) {
+    @PostMapping("/login")
+    public String loginSession(@ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+
+        if(bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비번이 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //Login Success
+        //세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관
+       sessionManager.createSession(loginMember, response);
+
+        return "redirect:/";
+    }
+
+//    @PostMapping("/logout")
+    public String logoutCookie(HttpServletResponse response) {
         expireCookie(response, "memberId");
+
+        return "home";
+    }
+
+    @PostMapping("/logout")
+    public String logoutSession(HttpServletRequest request) {
+        sessionManager.expire(request);
 
         return "home";
     }
